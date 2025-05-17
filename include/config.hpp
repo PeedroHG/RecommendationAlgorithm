862717e12e846c1a5564520f824dec08da0b2630
Igor Peixoto <igorpeixoto_@outlook.com.brm>
2025-05-17 16:59:31 -0300

    Arquivo para definir configurações globais

---
+++ include/config.hpp
@@ -0,0 +1,13 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#pragma once
+#define MIN_AVALIACAO = 50;
+#define NUM_RECOMENDACOES_POR_USUARIO = 10;
+#define Num_VIZINHOS_SIMILARES = 5;
+
+
+
+
+
+#endif // CONFIG_HPP
